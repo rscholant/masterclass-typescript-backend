7dbe30c4e7864c24e31e04842afa74018019febc
Rafael Scholant <rafael.scholant@gmail.com>
2020-04-06 14:59:59 -0300

    first commit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
+
+const users = [{ name: 'Diego', email: 'diego@rocketseat.com.br' }];
+
+export default {
+  async index(req: Request, res: Response) {
+    return res.json(users);
+  },
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+    emailService.sendMail({
+      to: { name: 'Rafael Scholant', email: 'Rafael.scholant@gmail.com' },
+      message: { subject: 'Bem-vindo ao sistema', body: 'Seja bem-vindo' },
+    });
+  },
+};
